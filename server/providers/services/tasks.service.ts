6e189875e067707677928671a0c60eace04f4c84
Spencer Hall <spencer@hallblog.com>
2022-02-19 14:52:30 -0700

    Task Service File is Complete '

--- server/providers/services/tasks.service.ts
+++ server/providers/services/tasks.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { identity } from 'lodash';
-import { Project } from 'server/entities/project.entity';
 import { Task } from 'server/entities/task.entity';
 import { Repository } from 'typeorm';
 
+@Injectable()
 export class TasksService {
     constructor(
         @InjectRepository(Task)
@@ -23,5 +22,17 @@ export class TasksService {
 
     findTaskByID(id: number): Promise<Task> {
         return this.taskRepository.findOne(id);
+    }
+
+    async updateCompleted(id: number) {
+        let taskToUpdate = await this.taskRepository.findOne(id);
+        taskToUpdate.completed = true;
+        await this.taskRepository.save(taskToUpdate);
+    }
+
+    async assignUser(id: number, userid: number) {
+        let taskToupdate = await this.taskRepository.findOne(id);
+        taskToupdate.userId = userid;
+        await this.taskRepository.save(taskToupdate);
     }
 }
