ee11138959e5f327600fac3fc12c1dd3261b6b9a
Spencer Hall <spencer@hallblog.com>
2022-02-19 13:11:42 -0700

    Task service complete besides update methods. task controller has create method done.

--- server/controllers/tasks.controller.ts
+++ server/controllers/tasks.controller.ts
@@ -1,6 +1,30 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Param, Post } from "@nestjs/common";
+import { toNumber } from "lodash";
+import { JwtBody } from "server/decorators/jwt_body.decorator";
+import { JwtBodyDto } from "server/dto/jwt_body.dto";
+import { Task } from "server/entities/task.entity";
+import { TasksService } from "server/providers/services/tasks.service";
+
+class TaskPostBody {
+    name: string;
+    description: string;
+    time_estimation: number;
+}
 
 @Controller()
 export class TasksController{
-  
+  constructor(private tasksService: TasksService) {}
+
+  @Post('/projects/:project_id/tasks')
+  public async create(@Param('project_id') projectId: string, @JwtBody() jwtBody: JwtBodyDto, @Body() body: TaskPostBody) {
+      let task = new Task();
+      task.projectId = <number><unknown>projectId;
+      task.description = body.description;
+      task.completed = false;
+      task.time_estimation = body.time_estimation;
+      task.userId = jwtBody.userId;
+      task = await this.tasksService.createTask(task);
+      return { task };
+  }
+
 }
--- server/providers/services/tasks.service.ts
+++ server/providers/services/tasks.service.ts
@@ -1,5 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { identity } from 'lodash';
+import { Project } from 'server/entities/project.entity';
+import { Task } from 'server/entities/task.entity';
+import { Repository } from 'typeorm';
 
 export class TasksService {
-  
+    constructor(
+        @InjectRepository(Task)
+        private taskRepository: Repository<Task>
+    ) {}
+
+    createTask(task: Task): Promise<Task> {
+        return this.taskRepository.save(task);
+    }
+
+    findAllTasks(projectId: number): Promise<Task[]> {
+        return this.taskRepository.find({
+            where: { projectId },
+        });
+    }
+
+    findTaskByID(id: number): Promise<Task> {
+        return this.taskRepository.findOne(id);
+    }
 }
