6a093407c1911cba8c1a9a56280719bb8cfde264
Levi Nielson <levi.nielson01@gmail.com>
2022-02-22 18:11:54 -0700

    Fix relationship for project and task when posting

--- server/controllers/tasks.controller.ts
+++ server/controllers/tasks.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Param, Patch, Post } from "@nestjs/common";
+import { Console } from "console";
 import { JwtBody } from "server/decorators/jwt_body.decorator";
 import { JwtBodyDto } from "server/dto/jwt_body.dto";
 import { Task } from "server/entities/task.entity";
@@ -7,7 +8,7 @@ import { TasksService } from "server/providers/services/tasks.service";
 class TaskPostBody {
     name: string;
     description: string;
-    time_estimation: number;
+    time_estimation: string;
 }
 
 @Controller()
@@ -17,10 +18,11 @@ export class TasksController{
   @Post('/projects/:project_id/tasks')
   public async create(@Param('project_id') projectId: string, @JwtBody() jwtBody: JwtBodyDto, @Body() body: TaskPostBody) {
       let task = new Task();
-      task.projectId = <number><unknown>projectId;
+      task.title = body.name;
+      task.projectId = parseInt(projectId);
       task.description = body.description;
       task.completed = false;
-      task.time_estimation = body.time_estimation;
+      task.timeEstimation = parseInt(body.time_estimation);
       task.userId = jwtBody.userId;
       task = await this.tasksService.createTask(task);
       return { task };
